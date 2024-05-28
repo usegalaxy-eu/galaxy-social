de2bfbd38e132f3d7ae32ccdd89758f3d07d8bac
Arash <arash77.kad@gmail.com>
2024-05-28 16:02:16 +0200

    make comment_text shorter

--- lib/plugins/markdown.py
+++ lib/plugins/markdown.py
@@ -28,11 +28,9 @@ class markdown_client:
             text = f"{content}{mentions}{hashtags}{_images}"
             if self.save_path:
                 os.makedirs(self.save_path, exist_ok=True)
-                prefix = (
-                    kwargs.get("file_path", "").replace("/", "-").replace(".md", "")
-                )
+                prefix = kwargs.get("file_path", "").replace(".md", "")
                 file_name = (
-                    f"{self.save_path}/{prefix}_{time.strftime('%Y%m%d-%H%M%S')}.md"
+                    f"{self.save_path}/{prefix.replace('/', '-')}_{time.strftime('%Y%m%d-%H%M%S')}.md"
                 )
                 with open(file_name, "w") as f:
                     f.write(text)
@@ -43,7 +41,7 @@ class markdown_client:
                     "mastodon" in social_media or "bluesky" in social_media
                 ):
                     pre_comment_text = f"Please note that Mastodon and Bluesky only support up to 4 images in a single post. The first 4 images will be included in the post, and the rest will be ignored.\n"
-                comment_text = f"File: {kwargs.get('file_path')}\n{pre_comment_text}This is a preview that will be posted to {social_media}:\n\n{text}"
+                comment_text = f"{pre_comment_text}This is a preview from {prefix.split('/')[-1]} that will be posted to {social_media}:\n\n{text}"
                 return True, None, comment_text
             return True, None
         except Exception as e:
