8122d81bab81ebf2aa4aeeecba7ed801b133a8a4
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2024-07-17 10:41:30 +0200

    Use format stripping also for bluesky

--- lib/plugins/bluesky.py
+++ lib/plugins/bluesky.py
@@ -5,7 +5,8 @@ from typing import Dict, List, Optional, Tuple, cast
 import atproto
 import requests
 from bs4 import BeautifulSoup
-from markdown import markdown
+
+from .base import strip_markdown_formatting
 
 
 class bluesky_client:
@@ -200,10 +201,7 @@ class bluesky_client:
         # convert markdown formatting because bluesky doesn't support it
         paragraphs = content.split("\n\n\n")
         for i, p in enumerate(paragraphs):
-            soup = BeautifulSoup(markdown(p), "html.parser")
-            for link in soup.find_all("a"):
-                link.string = f"{link.string}: {link['href']}"
-            paragraphs[i] = "\n\n".join([p.get_text() for p in soup.find_all("p")])
+            paragraphs[i] = strip_markdown_formatting(p)
         content = "\n\n\n".join(paragraphs)
 
         content += "\n"
