c3baaf91bd410a154ac0738a5b9fa33c682badcc
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2024-07-17 10:32:44 +0200

    Handle blockquoting during stripping

--- lib/plugins/base.py
+++ lib/plugins/base.py
@@ -9,30 +9,52 @@ class HTMLFilter(HTMLParser):
     text = ""
     href = ""
     li_count = 0
+    inside_blockquote = False
+    last_start_tag = ""
 
     def handle_starttag(self, tag, attrs):
-        if tag == "ol":
+        if tag == "blockquote":
+            self.inside_blockquote = True
+        elif tag == "ol":
+            if self.inside_blockquote:
+                self.text += ">"
             self.li_count = 1
         elif tag == "ul":
+            if self.inside_blockquote:
+                self.text += ">"
             self.li_count = 0
         elif tag == "li":
+            if self.inside_blockquote:
+                self.text += "> "
             if self.li_count:
                 self.text += f"{self.li_count}. "
                 self.li_count += 1
             else:
                 self.text += "- "
-        elif tag == "p":
-            self.text = self.text.rstrip('\n') + "\n\n"
+        elif tag == "p" or (tag[0]=="h" and tag[1].isdigit()):
+            # add an empty line before paragraphs and headings
+            # do blockquoting as needed
+            if self.inside_blockquote:
+                if self.last_start_tag == "blockquote":
+                    p_sep = "\n\n> "
+                else:
+                    p_sep = "\n>\n> "
+            else:
+                p_sep = "\n\n"
+            self.text = self.text.rstrip("\n") + p_sep
         elif tag == "a":
             for name, value in attrs:
                 if name == "href":
                     self.href = value
                     break
+        self.last_start_tag = tag
 
     def handle_endtag(self, tag):
         if tag == "a":
             self.text += f": {self.href}"
             self.href = ""
+        elif tag == "blockquote":
+            self.inside_blockquote = False
 
     def handle_data(self, data):
         self.text += data
